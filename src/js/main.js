6b144cb499469f47d011e6a655dc38381dc8daa8
Feri Irawan <feriirawans1998@gmail.com>
2022-10-17 11:10:03 +0800

    feat: maker page

---
+++ src/js/main.js
@@ -0,0 +1,91 @@
+// Background Particles
+particlesJS.load('bg-particles', '/src/js/particles.json')
+particlesJS.load('footer-particles', '/src/js/particles.json')
+
+//  Dark Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkMode()
+}
+
+function darkMode() {
+  let icon = ''
+  let toggles = document.querySelectorAll('.toggle-dark')
+  let isDark = document.querySelector('html').classList.toggle('dark')
+
+  if (isDark) {
+    icon = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-sun text-white" viewBox="0 0 16 16">
+        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+      </svg>`
+
+    localStorage.setItem('theme', 'dark')
+  } else {
+    icon = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+        <path
+          d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"
+        />
+      </svg>`
+
+    localStorage.removeItem('theme')
+  }
+
+  toggles.forEach((toggle) => {
+    toggle.innerHTML = icon
+  })
+}
+
+// Membuat Badge
+const form = document.querySelector('#badge-generator')
+form.onsubmit = (e) => {
+  e.preventDefault()
+  const input = e.target.elements
+  const username = encodeURI(input.username.value)
+  const repository = encodeURI(input.repository.value)
+  const label = encodeURI(input.label.value || 'VISITOR')
+  const style = encodeURI(input.style.value || 'flat-square')
+  const color = input.color.value.replace('#', '%23')
+  const token = encodeURI(input.token.value || '')
+
+  let endpoint = `https://visitor-badges.glitch.me?username=${username}&repo=${repository}&label=${label}&style=${style}&color=${color}`
+  if (token !== '') {
+    endpoint += `&token=${token}`
+  }
+
+  const urlBadge = document.querySelector('#url-badge')
+  const badgeContainer = document.querySelector('#badge')
+  const htmlCode = document.querySelector('#html-code')
+  const mdCode = document.querySelector('#md-code')
+
+  badgeContainer.src = endpoint + '&contentType=svg'
+  urlBadge.value = endpoint
+
+  htmlCode.innerHTML = `&lt;img alt=&quot;Visitor Badge&quot; src=&quot;${endpoint}&contentType=svg&quot;&gt;`
+  mdCode.innerHTML = `![Visitor Badge](${endpoint}&contentType=svg)`
+  hljs.highlightAll()
+}
+
+// Clipboard
+copy('.btn-copy')
+function copy(selector) {
+  const clipboard = new ClipboardJS(selector)
+  clipboard.on('success', function (e) {
+    e.trigger.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+      </svg>`
+
+    e.clearSelection()
+  })
+
+  clipboard.on('error', function (e) {
+    e.trigger.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-danger" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z"/>
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+      </svg>`
+  })
+}
